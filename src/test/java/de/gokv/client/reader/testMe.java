5613426300d314cbe62921596d5f089f33eeb05d
Christoph Kiank <christoph.kiank@bitmarck.de>
2017-05-16 09:33:08 +0200

    Ausgabe: Titel[] und Inhalt[] nebeneinander in der Ausgabe

--- src/test/java/de/gokv/client/reader/testMe.java
+++ src/test/java/de/gokv/client/reader/testMe.java
@@ -7,34 +7,35 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class testMe {
-	static int j=0;
+	static int j = 1;
 
 	public static void main(String[] args) throws IOException {
+
+		String zeile = "";
+		String titel[];
 		BufferedReader datei = null;
-		ReadCSVDir ordnerListe = new ReadCSVDir();
-			
+
 		try {
-			ordnerListe.datei();
-			datei = new BufferedReader(new FileReader(new File("C:/java/workspaces/BitgoKV/gokv-client-main/test-output/" + ordnerListe.datei)));
-			String zeile = "";
-			while (null !=(zeile=datei.readLine())) {
-				System.out.println(">>> Zeile " + j++ +" <<<");
+
+			datei = new BufferedReader(new FileReader(new File(
+					"C:/java/workspaces/BitgoKV/gokv-client-main/test-output/famv_direct_input_monitoring_20170515131131.csv")));
+			// die Titelzeile in extra Array speichern, damit die Werte aus den
+			// Zeile darunter zugeordnet werden können.
+			titel = datei.readLine().split(";");
+			while (null != (zeile = datei.readLine())) {
+				System.out.println(">>>>>>>>>>>>>>>>>>>>>>> Zeile " + j++ + " <<<<<<<<<<<<<<<<<<<<<<<<");
 				String[] split = zeile.split(";");
 				for (int i = 0; i < split.length; i++) {
-					System.out.println(split[i]);
+					System.out.println(titel[i] + ": " + split[i]);
 				}
 				System.out.println("-----------------------------------");
 			}
-						
+
 		} catch (FileNotFoundException e) {
 			System.out.println("Datei nicht gefunden");
 			e.printStackTrace();
 		}
-		
-		
-			
-		}
-		
+
 	}
 
-
+}
