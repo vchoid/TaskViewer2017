c44e43b0518cc0c23af27991dd49dcc590973041
Christoph Kiank <christoph.kiank@bitmarck.de>
2017-05-16 09:15:57 +0200

    Stand ReadCSVDir ließt den Ordner, aber zeigt nur letzte Datei testMe ließt ausgwählte CSV (von ReadCSVDir) und zeigt Inhalt an.

--- src/test/java/de/gokv/client/reader/ReadCSVDir.java
+++ src/test/java/de/gokv/client/reader/ReadCSVDir.java
@@ -8,35 +8,32 @@ public class ReadCSVDir {
 
 	String[] dateiListe;
 	String datei;
-	
-	
-// Liste der Datein im Ordner.
+
+	// Liste der Datein im Ordner.
 	public String dateiListe() {
 		File dateien = new File("C:/java/workspaces/BitgoKV/gokv-client-main/test-output");
 		dateiListe = dateien.list();
 		return dateiListe + "\nGesamtanzahl Datein: " + dateiListe.length;
 	}
-	
-	public String datei(){
+
+	public String datei() {
 		dateiListe();
 		for (int i = 0; i < dateiListe.length; i++) {
-			if(dateiListe[i].startsWith("famv_direct_input_")){
-			datei = dateiListe[i];
-			
-			}
-			else{
+			if (dateiListe[i].contains("_monitoring_")) {
+				datei = dateiListe[i];
+				System.out.println(datei);
+			} else if (dateiListe[i].contains("_output_")) {
 				datei = "keine CSV Dateien vorhanden!";
 			}
 		}
-		return datei ;
+		System.out.println(datei);
+		return datei;
 	}
 
-	
-	
 	public static void main(String[] args) {
 		ReadCSVDir dir = new ReadCSVDir();
 		System.out.println(dir.datei());
-	
+
 	}
 
 }
