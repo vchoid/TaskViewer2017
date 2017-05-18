fd7b48f5e3083bd5a5a2a0de551668411e604299
Christoph Kiank <christoph.kiank@bitmarck.de>
2017-05-18 15:02:23 +0200

    ..

--- src/main/java/de/gokv/client/reader/TestMeInMain.java
+++ src/main/java/de/gokv/client/reader/TestMeInMain.java
@@ -5,7 +5,7 @@ public class TestMeInMain {
 	public static void main(String[] args) {
 		
 		// 
-		CSVReader csv = new CSVReader("C:/java/workspaces/BitgoKV/gokv-client-task-viewer/src/main/resources/famv_direct_input_monitoring_20170515131131_invalid.csv");
+		CSVReader csv = new CSVReader("../gokv-client-task-viewer/src/main/resources/famv_direct_input_monitoring_20170515131131_invalid.csv");
 		csv.readCsvFile();
 		
 		
