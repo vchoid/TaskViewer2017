091f773d5d8eea31f9796da0e933daae5203d860
Christoph Kiank <christoph.kiank@bitmarck.de>
2017-05-22 15:23:59 +0200

    funktioniert wieder

--- src/main/java/de/gokv/client/reader/TestMeInMain.java
+++ src/main/java/de/gokv/client/reader/TestMeInMain.java
@@ -9,15 +9,15 @@ public class TestMeInMain {
 				+ "/src/test/resources/invalid_famv_direct_input_monitoring_20170515131131.csv");
 		reader.readCsvFile();
 
-//		for (Task task : reader.getTasks()) {
-//			System.out.println(task);
-//		}
-//
-//		// gebDat
-//		String gebDat;
-//		for (int i = 0; i < reader.getTasks().size(); i++) {
-//			gebDat = reader.getTasks().get(i).getGeb_dat();
-//			System.out.println(gebDat);
-//		}
+		for (Task task : reader.getTasks()) {
+			System.out.println(task);
+		}
+
+		// gebDat
+		String gebDat;
+		for (int i = 0; i < reader.getTasks().size(); i++) {
+			gebDat = reader.getTasks().get(i).getGeb_dat();
+			System.out.println(gebDat);
+		}
 	}
 }
