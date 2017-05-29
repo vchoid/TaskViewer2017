98dc62d1877d310c2811427d9af1d48d23717deb
Christoph Kiank <christoph.kiank@bitmarck.de>
2017-05-29 10:24:52 +0200

    test namen angepasst

--- src/test/java/de/gokv/client/taskviewer/HexaIdUtilTest.java
+++ src/test/java/de/gokv/client/taskviewer/HexaIdUtilTest.java
@@ -8,32 +8,32 @@ import de.gokv.client.reader.HexaIdUtil;
 public class HexaIdUtilTest {
 
 	@Test
-	public void testValidId() {
+	public void testValidHexaId() {
 		Assert.assertTrue("...gültige Id.", HexaIdUtil.isIdValid("405750F0395F11E7BED7F726E269B87F"));
 	}
 		
 	@Test
-	public void testInvalidId() {
+	public void testInvalidHexaId() {
 		Assert.assertFalse("...ungültige Id.", HexaIdUtil.isIdValid("405750F0395F11E7BED7F726E269B87G"));
 	}
 	
 	@Test
-	public void testTooLongValidId() {
+	public void testTooLongValidHexaId() {
 		Assert.assertFalse("...gültige Id, aber zu lang.", HexaIdUtil.isIdValid("405750F0395F11E7BED7F726E269B87FFFF"));
 	}
 	
 	@Test
-	public void testTooLongInvalidId() {
+	public void testTooLongInvalidHexaId() {
 		Assert.assertFalse("...ungültige Id und zu lang.", HexaIdUtil.isIdValid("405750F0395F11E7BED7F726E269B87FFGG"));
 	}
 	
 	@Test
-	public void testTooShortValidId() {
+	public void testTooShortValidHexaId() {
 		Assert.assertFalse("...gültige Id, aber zu kurz.", HexaIdUtil.isIdValid("405750F0395F11E7BED7F726E269B"));
 	}
 	
 	@Test
-	public void testTooShortInvalidId() {
+	public void testTooShortInvalidHexaId() {
 		Assert.assertFalse("...ungültige Id und zu kurz.", HexaIdUtil.isIdValid("405750F0395F11E7BED7F726E269G"));
 	}
 	
