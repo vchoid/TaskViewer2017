d44367caa04cc464a342b7a619227f72299c94e2
Christoph Kiank <christoph.kiank@bitmarck.de>
2017-05-30 12:09:07 +0200

    JavaDoc DateUtil fertig gemacht

--- src/main/java/Utils/HexaIdUtil.java
+++ src/main/java/Utils/HexaIdUtil.java
@@ -2,9 +2,21 @@ package Utils;
 
 import java.util.regex.Pattern;
 
-
+/**
+ * Überprüft ob ein {@link String} im Hexadezimal-Format vorliegt
+ * 
+ * @author Christoph Kiank
+ * @version 1.0.0
+ * 
+ *
+ */
 public class HexaIdUtil {
 
+	/**
+	 * 
+	 * @param id
+	 * @return 
+	 */
 	public static boolean isIdValid(String id) {
 
 		Pattern p = Pattern.compile("[A-F0-9]{32}");
