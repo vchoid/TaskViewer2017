87e671bb395696f9a9a4604363811986d55a97f1
Christoph Kiank <christoph.kiank@bitmarck.de>
2017-07-04 10:00:01 +0200

    ..

--- src/main/java/de/gokv/client/taskviewer/view/Pattern_PlaceholderTextField.java
+++ src/main/java/de/gokv/client/taskviewer/view/Pattern_PlaceholderTextField.java
@@ -22,43 +22,36 @@ public class Pattern_PlaceholderTextField extends JTextField {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private Color colorPlaceholderTxt = new HexaToRGB("color.placeholder.text").parseHexToRGB();
+	private static final Color COLOR_PLACEHOLDER_TEXT = new HexaToRGB("color.placeholder.text").parseHexToRGB();
+	private static final Font FONT_TEXT = new Font("Arial", Font.PLAIN, 15);
 
 	private String placeholder;
 	private Graphics2D g;
 	private int lenght;
-	Font txt = new Font("Arial", Font.PLAIN, 15);
 
 	public Pattern_PlaceholderTextField() {
 	}
 
 	public Pattern_PlaceholderTextField(String pTitle) {
-
 	}
-
 	@Override
 	protected void paintComponent(Graphics pG) {
 		super.paintComponent(pG);
-
 		if (placeholder.length() == 0 || getText().length() > 0) {
 			return;
 		}
 		setSize(lenght, 23);
-		setFont(txt);
+		setFont(FONT_TEXT);
 		g = (Graphics2D) pG;
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		g.setColor(colorPlaceholderTxt);
+		g.setColor(COLOR_PLACEHOLDER_TEXT);
 		g.drawString(placeholder, getInsets().left + 3, pG.getFontMetrics().getMaxAscent() + 3 + getInsets().top);
-
 	}
-
 	public void setPlaceholder(String s) {
 		placeholder = s;
 	}
-
 	public void setPlaceholder(String s, int lenght) {
 		this.lenght = lenght;
 		placeholder = s;
 	}
-
 }
